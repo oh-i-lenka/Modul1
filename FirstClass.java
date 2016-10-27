6d30659ed24cbb483020859b035975f353e65dd8
Olena <oh-i-lenka@gavatyuk.org.ua>
2016-10-27 09:54:57 +0200

    Add files Mod1

---
+++ FirstClass.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Olena on 27.10.2016.
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+
+}
---
+++ ThirdClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Olena on 27.10.2016.
+ */
+public class ThirdClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
